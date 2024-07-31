7330c1edde6750b1a052f2757cda4addf011a6ec
BalmerDX <balmer@inbox.ru>
2024-07-31 08:56:30 +0200

    мелкие изменения

--- nn_speed_test.py
+++ nn_speed_test.py
@@ -2,13 +2,15 @@ from sudoku_stuff import *
 from sudoku_nn import *
 from time import time
 
+#попробовать квантизацию
 dtype=torch.float32
 device = torch.device("cuda", 0)
 #device = torch.device("cpu", 0)
 batch_size = 1000
 
 start_time = time()
-sudoku = get_puzzle("data/puzzles0_kaggle", idx='all')
+#sudoku = get_puzzle("data/puzzles0_kaggle", idx='all')
+sudoku = get_puzzle("data/puzzles2_17_clue", idx='all')
 load_time = time()-start_time
 print(len(sudoku), f"load time = {load_time} sec")
 
@@ -21,40 +23,8 @@ for offset in range(0, len(sudoku), batch_size):
 sudoku_to_mask_time = time()-start_time
 print(f"to mask = {sudoku_to_mask_time} sec")
 
-class SudokuPasses(nn.Module):
-    def __init__(self, passes=4):
-        super(SudokuPasses,self).__init__()
-        self.passes = passes
-        self.remove_box = SudokuFilterHVBox("box", dtype=dtype, device=device).to(device)
-        self.uniq_h = SudokuUniqueHVBox("h", dtype=dtype, device=device).to(device)
-        self.uniq_v = SudokuUniqueHVBox("v", dtype=dtype, device=device).to(device)
-        self.uniq_box = SudokuUniqueHVBox("box", dtype=dtype, device=device).to(device)
-        self.sudoku_equal = SudokuIsEqual(dtype=dtype, device=device).to(device)
-        self.digits_in_one_line_at_box_h = SudokuDigitsInOneLineAtBox("h", dtype=dtype, device=device).to(device)
-        self.digits_in_one_line_at_box_v = SudokuDigitsInOneLineAtBox("v", dtype=dtype, device=device).to(device)
-
-        self.doubles_h = SudokuDigitsDoubles("v", dtype=dtype, device=device).to(device)
-        self.doubles_v = SudokuDigitsDoubles("v", dtype=dtype, device=device).to(device)
-        self.doubles_box = SudokuDigitsDoubles("box", dtype=dtype, device=device).to(device)
-
-        self.sudoku_solved = SudokuSolved(dtype=dtype, device=device).to(device)
-
-    def forward(self, mask : torch.Tensor) -> torch.Tensor:
-        for _ in range(self.passes):
-            mask = self.remove_box(mask)
-            mask = self.digits_in_one_line_at_box_h(mask)
-            mask = self.digits_in_one_line_at_box_v(mask)
-            mask = self.uniq_h(mask)
-            mask = self.uniq_v(mask)
-            mask = self.uniq_box(mask)
-
-            #не требуется для решения puzzles0_kaggle
-            #mask = self.doubles_h(mask)
-            #mask = self.doubles_v(mask)
-            #mask = self.doubles_box(mask)
-        return mask, self.sudoku_solved(mask)
-
-passes = SudokuPasses()
+#passes = SudokuPasses(dtype, device, passes=4, remove_doubles=False) #puzzles0_kaggle
+passes = SudokuPasses(dtype, device, passes=16, remove_doubles=True) #puzzles2_17_clue
 passes.eval()
 
 with torch.no_grad():
@@ -63,14 +33,25 @@ with torch.no_grad():
     print(f"first sovled = {time()-start_time} sec")
 
     sum_not_solved = 0
+    mask_offset = 0
     for mask in masks:
         start_time = time()
         mask_solved, solved = passes(mask)
         solve_time = time()-start_time
 
         is_resolved, zeros_max = solved
-        count_solved = int(torch.sum(is_resolved.detach().cpu()).item())
-        print(f"{count_solved=}, {solve_time:0.3f} sec, {int(mask.shape[0]/solve_time)} sudoku/sec")
+        is_resolved = is_resolved.detach().cpu()
+        for i in range(is_resolved.shape[0]):
+            if is_resolved[i] < 1.:
+                print("not solved first idx=", i+mask_offset)
+                break
+
+        count_solved = int(torch.sum(is_resolved).item())
+        count_failed = int(torch.sum(zeros_max.detach().cpu()).item())
+        print(f"solved={count_solved}, failed={count_failed}, {solve_time:0.3f} sec, {int(mask.shape[0]/solve_time)} sudoku/sec")
         sum_not_solved += mask.shape[0]-count_solved
+
+        mask_offset += mask.shape[0]
         pass
-    print(f"{sum_not_solved=}")
+    percent_not_solved = sum_not_solved/len(sudoku)*100.
+    print(f"{sum_not_solved=} {percent_not_solved=:.1f}%")
--- sudoku_nn/sudoku_fun.py
+++ sudoku_nn/sudoku_fun.py
@@ -406,3 +406,42 @@ def sudoku_to_mask(sudoku, dtype):
         return conv_sudoku(input)
     else:
         assert 0
+
+class SudokuPasses(nn.Module):
+    '''
+    Класс, который может решать судоку за 1 проход.
+    Если конечно повезёт и судоку не очень сложное.
+    '''
+    def __init__(self, dtype, device, passes=4, remove_doubles=True):
+        super(SudokuPasses,self).__init__()
+        self.passes = passes
+        self.remove_doubles = remove_doubles
+        self.remove_box = SudokuFilterHVBox("box", dtype=dtype, device=device).to(device)
+        self.uniq_h = SudokuUniqueHVBox("h", dtype=dtype, device=device).to(device)
+        self.uniq_v = SudokuUniqueHVBox("v", dtype=dtype, device=device).to(device)
+        self.uniq_box = SudokuUniqueHVBox("box", dtype=dtype, device=device).to(device)
+        self.sudoku_equal = SudokuIsEqual(dtype=dtype, device=device).to(device)
+        self.digits_in_one_line_at_box_h = SudokuDigitsInOneLineAtBox("h", dtype=dtype, device=device).to(device)
+        self.digits_in_one_line_at_box_v = SudokuDigitsInOneLineAtBox("v", dtype=dtype, device=device).to(device)
+
+        self.doubles_h = SudokuDigitsDoubles("v", dtype=dtype, device=device).to(device)
+        self.doubles_v = SudokuDigitsDoubles("v", dtype=dtype, device=device).to(device)
+        self.doubles_box = SudokuDigitsDoubles("box", dtype=dtype, device=device).to(device)
+
+        self.sudoku_solved = SudokuSolved(dtype=dtype, device=device).to(device)
+
+    def forward(self, mask : torch.Tensor) -> torch.Tensor:
+        for _ in range(self.passes):
+            mask = self.remove_box(mask)
+            mask = self.digits_in_one_line_at_box_h(mask)
+            mask = self.digits_in_one_line_at_box_v(mask)
+            mask = self.uniq_h(mask)
+            mask = self.uniq_v(mask)
+            mask = self.uniq_box(mask)
+
+            #не требуется для решения puzzles0_kaggle
+            if self.remove_doubles:
+                mask = self.doubles_h(mask)
+                mask = self.doubles_v(mask)
+                mask = self.doubles_box(mask)
+        return mask, self.sudoku_solved(mask)
